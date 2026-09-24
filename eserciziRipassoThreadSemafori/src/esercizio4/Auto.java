43c11aaf0b6bf4900e92076c4b24fd524d4248a2
federico <federico.pedretti@buonarroti.tn.it>
2026-09-24 18:18:53 +0200

    Modificato es4

--- eserciziRipassoThreadSemafori/src/esercizio4/Auto.java
+++ eserciziRipassoThreadSemafori/src/esercizio4/Auto.java
@@ -5,11 +5,9 @@ import java.util.concurrent.Semaphore;
 
 public class Auto extends Thread{
 
-    private Semaphore semaphore;
     private String nome;
 
-    Auto(String nome, Semaphore semaphore){
-        this.semaphore = semaphore;
+    Auto(String nome){
         this.nome = nome;
     }
 
@@ -17,13 +15,32 @@ public class Auto extends Thread{
     public void run() {
         Random random = new Random();
         try {
-            semaphore.acquire();
 
-            System.out.println(nome + " entrata");
-            Thread.sleep(random.nextInt(4));
+            System.out.println(nome + " è arrivata");
 
-            semaphore.release();
-            System.out.println(nome + "uscita");
+            //Verifica che ci siano posti liberi
+            Main.postiLiberi.acquire();
+
+            //Adesso deve coordinarsi con le altre auto entrate per poter modificare la variabile postiOccupato
+            Main.mutex.acquire();
+
+            Main.postiOccupati++;
+
+            System.out.println(nome + " è entrata. Posti occupati: " + Main.postiOccupati);
+
+            //Libera la risorsa condivisa
+            Main.mutex.release();
+
+            Thread.sleep(random.nextInt(4)*1000);
+
+            //Verifica se può entrare ancora nella variabile condivisa a modificare la variabile dei posti occupati, prima di uscire
+            Main.mutex.acquire();
+            Main.postiOccupati--;
+            System.out.println(nome + " esce. Posti occupati: " + Main.postiOccupati);
+            Main.mutex.release();
+
+            //Libera il posto ad un'altra auto
+            Main.postiLiberi.release();
 
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
--- eserciziRipassoThreadSemafori/src/esercizio4/Main.java
+++ eserciziRipassoThreadSemafori/src/esercizio4/Main.java
@@ -3,12 +3,15 @@ package esercizio4;
 import java.util.concurrent.Semaphore;
 
 public class Main {
+
+    protected static Semaphore mutex = new Semaphore(1);
+    protected static Semaphore postiLiberi = new Semaphore(3);
+    protected static int postiOccupati = 0;
+
     public static void main(String[] args) {
 
-        Semaphore s = new Semaphore(3);
-
         for(int i=0; i<8; i++){
-            Auto auto = new Auto(("Autos"+i), s);
+            Auto auto = new Auto(("Auto"+i));
             auto.start();
         }
 
